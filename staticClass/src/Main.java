d67393df930bbd85836cd9e4257aa1c1517540cc
valer <taran.valeriya93@mail.ru>
2023-01-10 20:28:30 +0400

    removed errors

--- staticClass/src/Main.java
+++ staticClass/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(ActionsOnNumbers.sumNumbers(-6, -3, 2, 7, -1, 5));
-        ActionsOnNumbers.subtractionNumbers(4, 0, 6, 10, 4);
+        System.out.println(ActionsOnNumbers.subtractionNumbers(4, 0, 6, 10, 4));
         System.out.println(ActionsOnNumbers.multiplicationNumbers(4, 6, 2, 45, 8));
         System.out.println(ActionsOnNumbers.dividendNumbers(100, 5, 20, 2));
         System.out.println(ActionsOnNumbers.factorialNumbers(-3));
