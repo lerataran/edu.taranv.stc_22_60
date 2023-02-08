ef7fbd883879e55902d5216d2ad91246303bec8d
valer <taran.valeriya93@mail.ru>
2023-02-08 11:36:46 +0400

    update

--- lambdaExpressions/src/Sequence.java
+++ lambdaExpressions/src/Sequence.java
@@ -1,22 +1,20 @@
+import java.util.Arrays;
+
 public class Sequence {
     public static int[] filter(int[] array, ByCondition condition) {
-        int temp = array.length;
-        int a = 0;
+        int count = 0;
         int index;
-        int[] mas = new int[temp];
+        int[] mas = new int[array.length];
         for (int j : array) {
             index = 0;
             if (!condition.isOk(j)) {
                 index++;
             }
             if (index == 0) {
-                mas[a] = j;
-                a++;
-                temp--;
+                mas[count] = j;
+                count++;
             }
         }
-        int[] mass = new int[array.length - temp];
-        System.arraycopy(mas, 0, mass, 0, array.length - temp);
-        return mass;
+        return Arrays.copyOf(mas, count);
     }
 }
